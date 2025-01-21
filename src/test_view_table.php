13a14d2c4397916792c6c9b28a6ea9d9f2948d9f
st10685 <st10685@iismarconipieralisi.it>
2025-01-21 08:55:02 +0000

    modifiche

--- src/test_view_table.php
+++ src/test_view_table.php
@@ -1,2 +1,32 @@
 <?php
-require_once '../includes/db.php'
+require_once 'includes/db.php';
+
+$table = "users";
+$query = "SELECT * FROM $table";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0 )
+{
+    echo "<table>";
+
+    while($column = $result->fetch_field())
+    {
+        //var_dump($column);
+        echo "<th>";
+        echo $column->name;
+        echo "</th>";
+    }
+    // Stampo il contenuto della tabella
+    while ($row = $result->fetch_assoc())
+    {
+        // Inserimento delle tuple della tabella
+        
+        echo "<tr>";
+        foreach ($row as $key => $value)
+        {
+            echo "<td>$value</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";    
+}
